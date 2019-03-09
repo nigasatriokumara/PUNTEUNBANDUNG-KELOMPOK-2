401fa0bdd82bd6c07d24676d72ee4b8b5cd73867
Kevin Reynaldi <kreynaldi04@gmail.com>
2019-03-09 11:19:02 +0700

    Login Page Admin

---
+++ app/src/main/java/apps/sinterklas/punteunbandung/AdminLoginActivity.java
@@ -0,0 +1,13 @@
+package apps.sinterklas.punteunbandung;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AdminLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_login);
+    }
+}
